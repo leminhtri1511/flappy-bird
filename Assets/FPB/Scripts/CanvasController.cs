5f59d438ebfcb192e3f749e48f2880e10d6fe201
leminhtri1511 <leminhtri151102@gmail.com>
2026-09-23 16:32:10 +0700

    feat: make UI responsive

---
+++ Assets/FPB/Scripts/CanvasController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FPB.Scripts
+{
+    public class CanvasController : MonoBehaviour
+    {
+        [SerializeField] private Canvas _canvas;
+        [SerializeField] private CanvasScaler _canvasScaler;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = _canvas.worldCamera;
+
+            ScreenSizeCalculate();
+        }
+
+        private void ScreenSizeCalculate()
+        {
+            var isTablet = _camera.aspect > (9f / 16f);
+
+
+            _canvasScaler.matchWidthOrHeight = isTablet ? 1f : 0;
+        }
+    }
+}
