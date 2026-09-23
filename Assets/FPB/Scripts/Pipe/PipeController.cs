b07794c560b5f4ba2f0b1a513dcdf13c490960b0
leminhtri1511 <leminhtri151102@gmail.com>
2026-09-23 15:57:45 +0700

    feat: setup scoring system

--- Assets/FPB/Scripts/Pipe/PipeController.cs
+++ Assets/FPB/Scripts/Pipe/PipeController.cs
@@ -1,4 +1,5 @@
 using FPB.Scripts.Manager;
+using FPB.Scripts.Score;
 using UnityEngine;
 
 namespace FPB.Scripts.Pipe
@@ -9,11 +10,22 @@ namespace FPB.Scripts.Pipe
         [SerializeField] private float _hideXPosition = -10f;
 
         private PipeSpawner _pipeSpawner;
+        private ScoreZone _scoreZone;
+
+        private void Awake()
+        {
+            _scoreZone = GetComponentInChildren<ScoreZone>();
+        }
+
+        private void OnEnable()
+        {
+            _scoreZone.ResetScoreZone();
+        }
 
         private void Update()
         {
             if (GameManager.Instance.GameState != GameState.Playing) return;
-            
+
             transform.position += Vector3.left * (_moveSpeed * Time.deltaTime);
 
             if (transform.position.x < _hideXPosition)
