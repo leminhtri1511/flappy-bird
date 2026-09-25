143d06285c4f29256fc12731ea2aec89e71e301f
leminhtri1511 <leminhtri151102@gmail.com>
2026-09-25 15:45:30 +0700

    style: responsive UI

---
+++ Assets/FPB/Scripts/GlobalCanvas.cs
@@ -0,0 +1,33 @@
+﻿namespace FPB.Scripts
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class GlobalCanvas : MonoBehaviour
+    {
+        public static GlobalCanvas Instance;
+        [SerializeField] private Canvas _globalCanvas;
+        [SerializeField] private CanvasScaler _canvasScaler;
+        public Canvas Canvas => _globalCanvas;
+        public CanvasScaler CanvasScaler => _canvasScaler;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        private void Start()
+        {
+            SetDefaultFPS();
+        }
+
+        private void SetDefaultFPS()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+        }
+    }
+}
